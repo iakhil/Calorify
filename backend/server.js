f451c9085b1e85d9b51dd2c3de4064f933233e0f
Akhil Ivaturi <akhilivaturi@Akhils-Laptop.lan>
2024-12-05 19:57:28 -0600

    Make final changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import CORS
 const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
@@ -9,6 +10,13 @@ const PORT = 4000;
 
 // Middleware
 app.use(bodyParser.json());
+
+// Enable CORS
+app.use(cors({
+  origin: ['http://localhost:8081', 'http://192.168.1.67:8081'], // Replace with the frontend's address
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // If you're using cookies or authentication headers
+}));
 
 // Connect to MongoDB
 mongoose
